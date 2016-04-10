f0d1ae1f5cbdb3bbf8ab9fb74bc50f4ff7493654
Ludvik Haltuf <ludvik.haltuf@gmail.com>
2016-04-10 21:46:27 +0200

    First take on RodneCislo parser

--- src/main/java/net/haltuf/rc_validate/RodneCislo.java
+++ src/main/java/net/haltuf/rc_validate/RodneCislo.java
@@ -1,5 +1,9 @@
 package net.haltuf.rc_validate;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 /**
  * Validates and provides information about rodne cislo.
  * 
@@ -19,9 +23,100 @@ package net.haltuf.rc_validate;
  */
 public class RodneCislo {
 
+	/**
+	 * Supported format of rodne cislo.
+	 */
+	public static final String SUPPORTED_FORMAT = "\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d";
+
+	/**
+	 * Gender obtained from rodneCislo
+	 */
+	public enum Gender {
+		MALE, FEMALE
+	}
+
+	/**
+	 * rodne cislo in full form (10 digits)
+	 */
 	private final String rodneCislo;
 
+	/**
+	 * last 2 digits of year of birth, taken from rodneCislo
+	 */
+	private final int birthYear;
+
+	/**
+	 * month of birth, taken from rodneCislo
+	 */
+	private final int birthMonth;
+
+	/**
+	 * day of birth, taken from rodneCislo
+	 */
+	private final int birthDay;
+
+	/**
+	 * 4 digit suffix, taken from rodneCislo
+	 */
+	private final String birthSuffix;
+
+	/**
+	 * Gender recognized from rodneCislo
+	 */
+	private final Gender gender;
+
 	public RodneCislo(String rodneCislo) {
-		// TODO Auto-generated constructor stub
+		if (rodneCislo == null) {
+			throw new IllegalArgumentException("rodneCislo cannot be null");
+		}
+		this.rodneCislo = rodneCislo;
+
+		// je desetimístné číslo
+		if (!rodneCislo.matches(SUPPORTED_FORMAT)) {
+			throw new IllegalArgumentException("rodneCislo is not 10digit number");
+		}
+		birthSuffix = rodneCislo.substring(8);
+
+		// je dělitelné jedenácti beze zbytku
+		try {
+			if (Long.valueOf(rodneCislo) % 11 != 0) {
+				throw new IllegalArgumentException("rodneCislo is not divisible by 11");
+			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("rodneCislo has unexpected number format");
+		}
+
+		try {
+			birthYear = Integer.valueOf(rodneCislo.substring(0, 2));
+			// allow only years from 54 until today TODO: externalize
+			if (birthYear < 54 && birthYear >= 16) {
+				throw new NumberFormatException();
+			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("rodneCislo has unexpected year");
+		}
+
+		try {
+			int birthMonth = Integer.valueOf(rodneCislo.substring(2, 4));
+			// allow only only 1-12 for males and 51-62 for females TODO: externalize
+			if (birthMonth >= 1 && birthMonth <= 12) {
+				gender = Gender.MALE;
+			} else if (birthMonth >= 51 && birthMonth <= 62) {
+				gender = Gender.FEMALE;
+				birthMonth = birthMonth - 50;
+			} else {
+				throw new NumberFormatException();
+			}
+			this.birthMonth = birthMonth;
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("rodneCislo has unexpected month");
+		}
+
+		try {
+			birthDay = Integer.valueOf(rodneCislo.substring(4, 6));
+			LocalDate.parse(rodneCislo.substring(0,6), DateTimeFormatter.ofPattern("yyMMdd"));
+		} catch (NumberFormatException | DateTimeParseException e) {
+			throw new IllegalArgumentException("rodneCislo has unexpected day");
+		}
 	}
 }
---
+++ src/test/java/net/haltuf/rc_validate/RodneCisloTest.java
@@ -0,0 +1,14 @@
+package net.haltuf.rc_validate;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RodneCisloTest {
+
+	@Test
+	public void testRodneCislo() {
+		new RodneCislo("8406240106");
+	}
+
+}
